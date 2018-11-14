50a6946d92485cdcb3375cdcfeca1c013ef7f653
qbix0r <qbixx2@gmail.com>
2018-11-14 15:58:58 +0100

    example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GridsterConfig, GridsterItem} from 'angular-gridster2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'gridster-test';
+export class AppComponent implements OnInit {
+  optionsSelect: Array<any>;
+
+  options: GridsterConfig;
+  dashboard: Array<GridsterItem>;
+
+  foods: any[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
+  static itemChange(item, itemComponent) {
+    console.info('itemChanged', item, itemComponent);
+  }
+
+  static itemResize(item, itemComponent) {
+    console.info('itemResized', item, itemComponent);
+  }
+
+  ngOnInit() {
+    this.options = {
+      itemChangeCallback: AppComponent.itemChange,
+      itemResizeCallback: AppComponent.itemResize,
+      fixedRowHeight: 100,
+      gridType: "verticalFixed"
+    };
+
+    this.dashboard = [
+      {cols: 2, rows: 1, y: 0, x: 0},
+      {cols: 2, rows: 2, y: 1, x: 0}
+    ];
+
+
+    this.optionsSelect = [
+      { value: '1', label: 'Option 1' },
+      { value: '2', label: 'Option 2' },
+      { value: '3', label: 'Option 3' },
+    ];
+  }
+
+  changedOptions() {
+    this.options.api.optionsChanged();
+  }
+
+  removeItem(item) {
+    this.dashboard.splice(this.dashboard.indexOf(item), 1);
+  }
+
+  addItem() {
+    //this.dashboard.push({});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from '@angular/material';
+import {GridsterModule} from 'angular-gridster2';
+import {MDBBootstrapModulesPro} from 'ng-uikit-pro-standard';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    GridsterModule,
+    MDBBootstrapModulesPro.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
